3ed787d14973f17067bdf1eb1775fa9b102f857a
Viswa vardhan chary <viswavardhanchary@gmail.com>
2025-05-24 16:02:26 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const router = require('./routers/data-ai');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://makeme-v60n.onrender.com/'
 }));
 
 app.use("/data-ai" , router);
